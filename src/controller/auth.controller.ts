207e3beee644ae7612bde143d904ea619eae418c
akdfkeks <akdfkeks@gmail.com>
2023-01-02 15:51:48 +0900

    add: 로그인 로직 구현

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from '../guard/local-auth.guard';
+import { LocalAuthGuard } from '../provider/guard/local-auth.guard';
 import { AuthService } from '../service/auth.service';
 
 @Controller('auth')
@@ -9,6 +9,12 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return this.authService.login(req.user);
+    const login = await this.authService.login(req.user);
+    return {
+      message: '로그인에 성공하였습니다.',
+      data: {
+        accessToken: login.access_token,
+      },
+    };
   }
 }
