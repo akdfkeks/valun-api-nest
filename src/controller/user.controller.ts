a0fcc0c8c4b6b1141085180cf2d1d56e45f1ed91
akdfkeks <akdfkeks@gmail.com>
2023-01-02 15:52:22 +0900

    add: 회원가입

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,10 +1,28 @@
-import { Controller, UseGuards, Request, Get } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/guard/jwt-auth.guard';
+import {
+  Controller,
+  UseGuards,
+  Request,
+  Get,
+  Post,
+  Body,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/dto/user.dto';
+import { JwtAuthGuard } from 'src/provider/guard/jwt-auth.guard';
 import { UserService } from 'src/service/user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Post('signup')
+  async userSignup(@Body() userDto: CreateUserDto) {
+    const result = await this.userService.signUp(userDto);
+    if (result)
+      return {
+        message: '회원가입에 성공하였습니다.',
+        data: { id: result.id, nick: result.nick },
+      };
+  }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
