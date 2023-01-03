917d7c18c64660517b7e2cf2b0db0184b1b8cc2e
akdfkeks <akdfkeks@gmail.com>
2023-01-03 15:50:32 +0900

    .

--- src/service/sovle.service.ts
+++ src/service/sovle.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import SolutionRepository from 'src/repository/solution.repository';
 
 @Injectable()
-export class SolveService {}
+export class SolveService {
+  constructor(private readonly solutionRepository: SolutionRepository) {}
+
+  async createSolution() {
+    const solution = this.solutionRepository.create();
+  }
+}
