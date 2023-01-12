44d38dc7a0f805dd10f80c1712f1f58ac21d2eac
akdfkeks <akdfkeks@gmail.com>
2023-01-12 19:07:45 +0900

    fix: 사용 여부를 나타내는 플래그 누락

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -46,7 +46,9 @@ export class UserController {
     const user = await this.userService.findUniqueUserById(id);
     return {
       message: user ? '이미 사용중인 ID 입니다.' : '사용 가능한 ID 입니다.',
-      data: '',
+      data: {
+        available: user ? false : true,
+      },
     };
   }
 
@@ -57,7 +59,9 @@ export class UserController {
       message: user
         ? '이미 사용중인 닉네임 입니다.'
         : '사용 가능한 닉네임 입니다.',
-      data: '',
+      data: {
+        available: user ? false : true,
+      },
     };
   }
 
